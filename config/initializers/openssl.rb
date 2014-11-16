5af89b39673a5bb2fe1042c9d93784aee8a6144d
Andrew White <andyw@pixeltrix.co.uk>
2014-11-16 21:13:35 +0000

    Enable JCE for unlimited strength encryption

---
+++ config/initializers/openssl.rb
@@ -0,0 +1,12 @@
+if RUBY_PLATFORM == 'java'
+  require 'java'
+  java_import 'java.lang.ClassNotFoundException'
+
+  begin
+    security_class = java.lang.Class.for_name('javax.crypto.JceSecurity')
+    restricted_field = security_class.get_declared_field('isRestricted')
+    restricted_field.accessible = true
+    restricted_field.set nil, false
+  rescue ClassNotFoundException => e
+  end
+end
